9a113a764d1aecacb1587c9899d42de7b7aefa30
Kainos Innovative Solutions <kainos.devteam1@gmail.com>
2022-01-31 10:10:31 +0530

    seethafinal

--- src/app/customer-create/customer-create.component.ts
+++ src/app/customer-create/customer-create.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { RestApiService } from "../shared/rest-api.service";
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 var event:string;
 var num2: any;
 var num1 = localStorage.getItem('isLoggedIn');
@@ -18,25 +21,44 @@ styleUrls: ['./customer-create.component.scss']
 })
 
 export class CustomerCreateComponent implements OnInit {
+  
 
-    
+
+types:any;
+profileform : any;
 @Input() customerDetails = { name: '', email: '', phone: num2, gender: '', doorno: '', street: '', area: '', Pincode: '', city: '', landmark: '' }
 
   opened = true;
   opened1 = false;
   
-constructor(
-public restApi: RestApiService,
-public router: Router
-) { }
+  constructor(
+    public router: Router,
+    private frmbuilder: FormBuilder,
+    private http: HttpClient,
+    public restApi: RestApiService) {
+
+      
+ }
+ 
 ngOnInit() { 
-    
+  const emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  const mobilePattern = "^((\\+91-?)|0)?[0-9]{10}$";
+  this.profileform = this.frmbuilder.group({
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    uploadimage: ['', Validators.required],
+    emailid: ['', [Validators.required, Validators.pattern(emailPattern)]],
+    mobileno:['', [Validators.required, Validators.pattern(mobilePattern)]],
+    // updateOn: 'blur'
+   
+    })    
 ;}
-addCustomer() {
-this.restApi.createCustomer(this.customerDetails).subscribe((data: {}) => {
-this.router.navigate(['/customercreate'])
-})
-}
+// addCustomer() {
+// this.restApi.createCustomer(this.customerDetails).subscribe((data: {}) => {
+// this.router.navigate(['/customercreate'])
+// })
+// }
+sendprofile(profileform: any){}
 
 }
 
---
+++ src/app/onlinebooking/onlinebooking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onlinebooking',
+  templateUrl: './onlinebooking.component.html',
+  styleUrls: ['./onlinebooking.component.scss']
+})
+export class OnlinebookingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
