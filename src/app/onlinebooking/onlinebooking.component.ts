4ca48daa304b89b0b5a9bcdfab288978b2b976c9
Kainos Innovative Solutions <kainos.devteam1@gmail.com>
2022-02-11 17:57:57 +0530

    abhi

--- src/app/onlinebooking/onlinebooking.component.ts
+++ src/app/onlinebooking/onlinebooking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from "../shared/rest-api.service";
 
 @Component({
   selector: 'app-onlinebooking',
@@ -7,13 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnlinebookingComponent implements OnInit {
 
-  constructor() {  }
+  displaydata: any;
+  displaydata1: any;
+
+  constructor(
+    public restApi: RestApiService
+  ) {  }
 
   ngOnInit(): void {
+    this.displaycartype();
   }
 
   cartype(){
     console.log("hiiiii1111");
+}
+
+displaycartype(){
+    
+    
+  return this.restApi.getcartype().subscribe((cartypedata: {}) => {
+    // alert(data)
+    this.displaydata = cartypedata;
+    this.displaydata1 = this.displaydata.data.type;
+    
+    console.log("data>>>>",this.displaydata1)
+  })
 }
 
 }
