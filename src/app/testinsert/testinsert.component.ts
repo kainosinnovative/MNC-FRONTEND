37138f332aea45d7abda58764debfa1da7d7c4f1
Kainos Innovative Solutions <kainos.devteam1@gmail.com>
2022-03-24 09:50:12 +0530

    see

--- src/app/testinsert/testinsert.component.ts
+++ src/app/testinsert/testinsert.component.ts
@@ -3,7 +3,7 @@ import { DataTableDirective } from 'angular-datatables';
 import { Subject } from 'rxjs';
 // import 'rxjs/add/operator/map';
 import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
-import { Chart } from 'angular-highcharts';
+
 // import { donutChartOptions } from '../helpers/donutChartOptions';
 import { areaChartOptions } from '../helpers/areaChartOptions';
 import { barChart } from '../helpers/barChart';
@@ -16,16 +16,13 @@ import { oneLineBar } from '../helpers/oneLineBar';
 })
 export class TestinsertComponent implements OnInit
 {
-  // chart = new Chart(donutChartOptions);
-  areaSplineChart = new Chart(areaChartOptions);
-  barChart = new Chart(barChart);
-  oneLineBar = new Chart(oneLineBar);
+  // 
 
 constructor(private http: HttpClient) {
-  
+
 }
 
 ngOnInit(): void {
- 
+
 }
 }
