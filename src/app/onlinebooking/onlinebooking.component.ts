0d8947683ad891ba12d5052c76a5156a2c3988a8
Kainos Innovative Solutions <kainos.devteam1@gmail.com>
2022-03-25 11:05:03 +0530

    abi

--- src/app/onlinebooking/onlinebooking.component.ts
+++ src/app/onlinebooking/onlinebooking.component.ts
@@ -407,7 +407,7 @@ changeBgColor(offer_id:any){
           this.counter = this.counter + 1;
 
         (<HTMLInputElement>document.getElementById(buttonid)).innerHTML = "Selected";
-      (<HTMLInputElement>document.getElementById(buttonid)).style.backgroundColor = "#1872f3";
+      (<HTMLInputElement>document.getElementById(buttonid)).style.backgroundColor = "rgb(24 114 242)";
        offer_amt = Number((<HTMLInputElement>document.getElementById(offer_totalid)).value);
         //alert(offer_amt);
         this.finalvalue = this.finalvalue +(offer_amt);
@@ -423,7 +423,7 @@ changeBgColor(offer_id:any){
         this.counter = this.counter - 1;
         // alert("count else>>"+this.counter);
       (<HTMLInputElement>document.getElementById(buttonid)).innerHTML = "Select";
-      (<HTMLInputElement>document.getElementById(buttonid)).style.backgroundColor = "#69a3f7";
+      (<HTMLInputElement>document.getElementById(buttonid)).style.backgroundColor = "#0c5578";
        offer_amt = Number((<HTMLInputElement>document.getElementById(offer_totalid)).value);
        this.finalvalue = this.finalvalue -(offer_amt);
        this.onlinebooking.controls.comboprice_total.setValue(this.finalvalue.toFixed());
@@ -446,7 +446,7 @@ selectbuttoncolor(service_id:any,indexval:any){
 
       if(selecttext === 'Select') { 
         (<HTMLInputElement>document.getElementById(currentserviceid)).innerHTML = "Selected";
-       (<HTMLInputElement>document.getElementById(currentserviceid)).style.backgroundColor = "#1872f3";
+       (<HTMLInputElement>document.getElementById(currentserviceid)).style.backgroundColor = "rgb(24 114 242)";
        var service_amt = Number((<HTMLInputElement>document.getElementById(service_totalid)).value);
        console.log(service_amt);
       this.totalvalue = this.totalvalue +(service_amt);
@@ -457,7 +457,7 @@ selectbuttoncolor(service_id:any,indexval:any){
        } else {
    
      (<HTMLInputElement>document.getElementById(currentserviceid)).innerHTML = "Select";
-     (<HTMLInputElement>document.getElementById(currentserviceid)).style.backgroundColor = "#69a3f7";
+     (<HTMLInputElement>document.getElementById(currentserviceid)).style.backgroundColor = "#0C5578"; 
      var service_amt = Number((<HTMLInputElement>document.getElementById(service_totalid)).value);
      this.totalvalue = this.totalvalue -(service_amt);
     //  (<HTMLInputElement>document.getElementById("totalamount")).value =  this.totalvalue.toFixed();
